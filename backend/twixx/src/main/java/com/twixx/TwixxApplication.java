782f4d41e4b96a01dbc3f3a98711cbcb0bf3249f
Kushal Khanna <kushal.khanna.2025@mumail.ie>
2024-12-06 13:57:29 +0000

    Initialized project structure for frontend and backend

---
+++ backend/twixx/src/main/java/com/twixx/TwixxApplication.java
@@ -0,0 +1,13 @@
+package com.twixx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwixxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwixxApplication.class, args);
+	}
+
+}
---
+++ frontend/twixx/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-6xl'>TWIXX</div>
+    </div>
+  );
+}
+
+export default App;
