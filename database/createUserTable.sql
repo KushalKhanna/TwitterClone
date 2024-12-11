4ada042ceaa736cac30996b9e62787c50a38277b
p250091 <corey.gendron.2025@mumail.ie>
2024-12-11 21:16:21 +0000

    Added username column to DB command

--- database/createUserTable.sql
+++ database/createUserTable.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS CS613Users CASCADE;
 
-CREATE TABLE CS613Users (user_id SERIAL, "first_name" VARCHAR(16), "sur_name" VARCHAR(16), "password" VARCHAR(48), "friends" json, PRIMARY KEY (user_id) );
+CREATE TABLE CS613Users (user_id SERIAL, "username" VARCHAR(16), "first_name" VARCHAR(16), "sur_name" VARCHAR(16), "password" VARCHAR(48), "friends" json, PRIMARY KEY (user_id) );
 
 -- Add Altar Table for data input
