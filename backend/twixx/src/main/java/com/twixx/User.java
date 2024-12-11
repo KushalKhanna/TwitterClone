56fc977750975968351c117581c0871e0de05146
p250091 <corey.gendron.2025@mumail.ie>
2024-12-11 21:15:00 +0000

    Added getters and setters to user.

--- backend/twixx/src/main/java/com/twixx/User.java
+++ backend/twixx/src/main/java/com/twixx/User.java
@@ -3,10 +3,12 @@ package com.twixx;
 public class User {
     private static User _instance;
     private int user_id;
+    private String username;
     private String firstName;
     private String surName;
     private String password;
 
+    //Singleton pattern constructor. I want to incorporate the user_id with this    
     private User()
     {
     }
@@ -19,6 +21,46 @@ public class User {
         return _instance;
     }
 
-
+    //Block of Getters and Setters for potential future use
+    public int getID()
+    {
+        return this.user_id;
+    }
+    public void setID(int id)
+    {
+        this.user_id = id;
+    }
+    public String getUsername()
+    {
+        return this.username;
+    }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getSurName()
+    {
+        return this.surName;
+    }
+    public void setSurName(String surName)
+    {
+        this.surName = surName;
+    }
+    public String setPassword()
+    {
+        return this.password;
+    }
+    public void getPassword(String password)
+    {
+        this.password = password;
+    }
 
 }
