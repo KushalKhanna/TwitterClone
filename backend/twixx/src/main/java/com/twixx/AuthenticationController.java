032f22693e6c0bd10f6f8709d8671c0645ff0dc8
p250091 <corey.gendron.2025@mumail.ie>
2024-12-12 10:17:04 +0000

    added code for authentication controller

--- backend/twixx/src/main/java/com/twixx/AuthenticationController.java
+++ backend/twixx/src/main/java/com/twixx/AuthenticationController.java
@@ -1,10 +1,18 @@
 package com.twixx;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 public class AuthenticationController {
-    
+    private AuthenticationService authenticationService;
+
+    public ResponseEntity<String> login( User loginRequest) {
+        boolean isAuthenticated = authenticationService.login(loginRequest.getUsername(), loginRequest.getPassword());
+
+        if (isAuthenticated) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(401).body("Invalid username or password");
+        }
+    }
     
 }
