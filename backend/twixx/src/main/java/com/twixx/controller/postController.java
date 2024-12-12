a766912de5b74a4d919e8292db9bcea133c93912
Allenvt <94548490+Allenvt@users.noreply.github.com>
2024-12-12 19:52:33 +0000

    Create postController.java

---
+++ backend/twixx/src/main/java/com/twixx/controller/postController.java
@@ -0,0 +1,30 @@
+package com.example.twixx.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.twixx.entity.post;
+import com.example.twixx.repository.postRepo;
+
+
+
+@RestController
+public class postController {
+
+    @Autowired
+    postRepo postRepo;
+   
+
+    @PostMapping("/addPost")
+    public void createPost(@RequestBody post data){
+        System.out.println("Received Post: " + data.getPostContent());
+        postRepo.save(data);
+
+
+    }
+
+}
