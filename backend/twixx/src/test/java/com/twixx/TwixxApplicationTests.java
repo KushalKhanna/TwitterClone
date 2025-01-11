48165c1106f446542bb1d970a517318ce94c0025
Kushal Khanna <kushal.khanna.2025@mumail.ie>
2025-01-11 17:27:49 +0000

    Full project with polling

---
+++ backend/twixx/src/test/java/com/twixx/TwixxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twixx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwixxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
